07bf346ae4f3d5227b1575a72888076027bc26c0
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-12-07 16:26:09 +0200

    WTF

--- x_configDB.php
+++ x_configDB.php
@@ -1,16 +1,26 @@
 <?php
 
-$dbhost = '***REMOVED***';
-$dbuser = '***REMOVED***';
-$dbpassword = '***REMOVED***';
-$dbname = 'viktorina';
-$port = 3306;
+// $dbhost = '***REMOVED***';
+// $dbuser = '***REMOVED***';
+// $dbpassword = '***REMOVED***';
+// $dbname = 'viktorina';
+// $port = 3306;
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+// $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpassword = '';
+$dbname = 'viktorina';
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-=======
-?>
+
