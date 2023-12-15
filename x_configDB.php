101e242199d282bbbe44b3b3ac65c42edafbedac
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-12-15 11:25:44 +0200

    V-6.4.4
    Some updates to header. Need to check and fix some errors
    
    Co-Authored-By: Aldas <37956008+brutall100@users.noreply.github.com>

--- x_configDB.php
+++ x_configDB.php
@@ -4,9 +4,9 @@
 // $dbuser = '***REMOVED***';
 // $dbpassword = '***REMOVED***';
 // $dbname = 'viktorina';
-// $port = 3306;
+// $dbport = 3306;
 
-// $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+// $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $dbport);
 
 // if (!$conn) {
 //     die("Connection failed: " . mysqli_connect_error());
